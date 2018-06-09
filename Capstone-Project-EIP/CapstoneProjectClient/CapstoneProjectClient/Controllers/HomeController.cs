d878cf8dcfb1b66b54a5d5d4bc63b7e7ff4d0741
binhnqse62065 <binhnqse62065@fpt.edu.vn>
2018-06-09 11:51:36 +0700

    + Tạo project client + clone template client vào

---
+++ Capstone-Project-EIP/CapstoneProjectClient/CapstoneProjectClient/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HmsService.Sdk;
+using HmsService.Models.Entities.Services;
+
+namespace CapstoneProjectClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EventApi eventApi = new EventApi();
+            var eventCurr = eventApi.BaseService.GetEventById(1);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
