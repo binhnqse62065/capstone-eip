5ab2f1d675409a0fe4b937002c2186f377eb898a
HUYHDLSE61921\HuyHDL <lehuydn111@gmail.com>
2018-06-11 15:41:10 +0700

    Home Client

--- Capstone-Project-EIP/CapstoneProjectClient/CapstoneProjectClient/Controllers/HomeController.cs
+++ Capstone-Project-EIP/CapstoneProjectClient/CapstoneProjectClient/Controllers/HomeController.cs
@@ -5,16 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using HmsService.Sdk;
 using HmsService.Models.Entities.Services;
+using HmsService.Models.Entities;
+
 
 namespace CapstoneProjectClient.Controllers
 {
     public class HomeController : Controller
     {
+            private HmsEntities db = new HmsEntities();
         public ActionResult Index()
         {
             EventApi eventApi = new EventApi();
             var eventCurr = eventApi.BaseService.GetEventById(1);
-            return View();
+            return View(eventCurr);
         }
 
         public ActionResult About()
