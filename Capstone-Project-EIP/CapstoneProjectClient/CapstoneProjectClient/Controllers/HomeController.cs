eaf28ae6896346042ba6f8539f0f3ab84fa32d06
HUYHDLSE61921\HuyHDL <lehuydn111@gmail.com>
2018-06-12 09:55:32 +0700

    KO merge

--- Capstone-Project-EIP/CapstoneProjectClient/CapstoneProjectClient/Controllers/HomeController.cs
+++ Capstone-Project-EIP/CapstoneProjectClient/CapstoneProjectClient/Controllers/HomeController.cs
@@ -5,19 +5,16 @@ using System.Web;
 using System.Web.Mvc;
 using HmsService.Sdk;
 using HmsService.Models.Entities.Services;
-using HmsService.Models.Entities;
-
 
 namespace CapstoneProjectClient.Controllers
 {
     public class HomeController : Controller
     {
-            private HmsEntities db = new HmsEntities();
         public ActionResult Index()
         {
             EventApi eventApi = new EventApi();
             var eventCurr = eventApi.BaseService.GetEventById(1);
-            return View(eventCurr);
+            return View();
         }
 
         public ActionResult About()
