28df901719b2f908f804d6fd78c360cc76b06db0
DESKTOP-SVO5V0S\Hai Anh <anhthse61488@fpt.edu.vn>
2018-06-18 17:28:53 +0700

    trang admin speaker : hien thi danh sach speaker, pop-up add va edit speaker trang admin sponsor : hien thi danh sach sponsor, pop-up add va edit sponsor

---
+++ Capstone-Project-EIP/CapstoneProjectAdmin/Controllers/SpeakerController.cs
@@ -0,0 +1,20 @@
+﻿using HmsService.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneProjectAdmin.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        // GET: Speaker
+        public ActionResult Index()
+        {
+            EventApi eventApi = new EventApi();
+            var cur = eventApi.BaseService.GetEventById(1);
+            return View(cur);
+        }
+    }
+}
---
+++ Capstone-Project-EIP/CapstoneProjectAdmin/Controllers/SponsorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HmsService.Sdk;
+
+namespace CapstoneProjectAdmin.Controllers
+{
+    public class SponsorController : Controller
+    {
+        // GET: Sponsor
+        public ActionResult Index()
+        {
+            EventApi eventApi = new EventApi();
+            var cur = eventApi.BaseService.GetEventById(1);
+            return View(cur);
+        }
+    }
+}
