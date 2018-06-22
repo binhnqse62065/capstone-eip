212992f9d96ff183d9018a04efb9669b5f9123bf
binhnqse62065 <binhnqse62065@fpt.edu.vn>
2018-06-22 13:14:21 +0700

    + Xong phần QA + Phần like còn chậm nếu chạy trên server + Thêm phần data table cho client

---
+++ Capstone-Project-EIP/CapstoneProjectAdmin/API/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using CapstoneProjectAdmin.Models;
+using HmsService.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CapstoneProjectAdmin.API
+{
+    [RoutePrefix("api/session")]
+    public class SessionController : ApiController
+    {
+        private HmsEntities db = new HmsEntities();
+
+        [Route("getAllSession")]
+        [HttpGet]
+        public HttpResponseMessage GetQuestions()
+        {
+            var session = db.Sessions.FirstOrDefault();
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new JsonContent(new
+                {
+                    success = true,
+                    message = "Add successful!",
+                    data = session
+                })
+            };
+        }
+    }
+}
--- Capstone-Project-EIP/CapstoneProjectAdmin/Global.asax.cs
+++ Capstone-Project-EIP/CapstoneProjectAdmin/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Routing;
 
 namespace CapstoneProjectAdmin
 {
@@ -13,6 +15,10 @@ namespace CapstoneProjectAdmin
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
