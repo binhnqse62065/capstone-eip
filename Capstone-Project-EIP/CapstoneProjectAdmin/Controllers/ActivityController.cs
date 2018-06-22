e5d595a896a5d2c824b3b57b2c12f7a7c5b27576
DESKTOP-SVO5V0S\Hai Anh <anhthse61488@fpt.edu.vn>
2018-06-22 15:01:55 +0700

    Hiện thông tin trang Session và Activity + Edit của từng trang Nút Add hiển thị ra pop-up trong trang Interaction và Livestream

---
+++ Capstone-Project-EIP/CapstoneProjectAdmin/Controllers/ActivityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HmsService.Models.Entities;
+using HmsService.Sdk;
+
+namespace CapstoneProjectAdmin.Controllers
+{
+    public class ActivityController : Controller
+    {
+        // GET: Activity
+        private HmsEntities db = new HmsEntities();
+        public ActionResult Index()
+        {
+            EventApi eventApi = new EventApi();
+            var cur = eventApi.BaseService.GetEventById(1);
+            return View(cur.Sessions.ElementAt(1).Activities.ElementAt(1));
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
