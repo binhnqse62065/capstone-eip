8414523e545f98926e31a8348888a99a5e6ab241
DESKTOP-SVO5V0S\Hai Anh <anhthse61488@fpt.edu.vn>
2018-06-28 14:09:19 +0700

    Trang activity: bỏ trang + nút edit, thêm nút add bằng pop-up Api controller và view model của voting và QA

---
+++ Capstone-Project-EIP/CapstoneProjectAdmin/API/QAAPIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HmsService.Sdk;
+using HmsService.Models.Entities;
+using CapstoneProjectAdmin.ViewModel;
+
+namespace CapstoneProjectAdmin.API
+{
+    [RoutePrefix("api/QA")]
+    public class QAAPIController : ApiController
+    {
+        private HmsEntities db = new HmsEntities();
+        [Route("getAllQA")]
+        [HttpGet]
+        public IEnumerable<QAViewModel> GetQAs()
+        {
+            var listQA = db.QAs.Where(a => a.QAId==1).ToList().Select(a => new QAViewModel
+            {
+                QAId = a.QAId,
+                Name = a.QAName
+                
+            });
+            return listQA;
+        }
+    }
+}
