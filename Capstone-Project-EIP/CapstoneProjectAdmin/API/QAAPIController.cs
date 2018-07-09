39009e7d7ad2b11bd793da6ceb48bd6beac736ce
HUYHDLSE61921\HuyHDL <lehuydn111@gmail.com>
2018-07-09 13:35:39 +0700

    Thêm trang login client + sửa admin QA

--- Capstone-Project-EIP/CapstoneProjectAdmin/API/QAAPIController.cs
+++ Capstone-Project-EIP/CapstoneProjectAdmin/API/QAAPIController.cs
@@ -15,17 +15,46 @@ namespace CapstoneProjectAdmin.API
     public class QAAPIController : ApiController
     {
         private HmsEntities db = new HmsEntities();
+
         [Route("getAllQA")]
         [HttpGet]
-        public IEnumerable<QAViewModel> GetQAs()
+        public IEnumerable<QA> GetQAs()
         {
-            var listQA = db.QAs.Where(a => a.QAId==1).ToList().Select(a => new QAViewModel
-            {
-                QAId = a.QAId,
-                Name = a.QAName
-                
-            });
+            var listQA = db.QAs.Where(a => a.EventId == 1).ToList();
             return listQA;
+        }
+
+        [Route("getAllQuestionComment")]
+        [HttpPost]
+        public HttpResponseMessage getAllQuestionComment(Question qa)
+        {
+            var listQA = db.Questions.Where(a => a.QAId == qa.QAId).OrderByDescending(s => s.CreateTime).Select(v => new QuestionViewModel
+            {
+                QuestionId = v.QuestionId,
+                QAId = v.QAId,
+                QuestionContent = v.QuestionContent,
+                Username = v.Username,
+                CreateTime = v.CreateTime,
+                NumberOfLike = v.NumberOfLike,
+                Comments = v.Comments.OrderByDescending(s => s.CreateTime).Select(s => new CommentsViewModel
+                {
+                    Username = s.Username,
+                    CommentId = s.CommentId,
+                    CommentContent = s.CommentContent,
+                    QuestionId = s.QuestionId,
+                    CreateTime = s.CreateTime,
+                    NumberOfLike = s.NumberOfLike
+                }),
+            });
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new JsonContent(new
+                {
+                    success = true,
+                    data = listQA
+                })
+            };
         }
 
         [Route("DeleteQuestion")]
@@ -34,7 +63,7 @@ namespace CapstoneProjectAdmin.API
         {
             var questionDelete = db.Questions.Find(question.QuestionId);
             var commentDeleteList = db.Comments.Where(e => e.QuestionId == question.QuestionId);
-            foreach(var item in commentDeleteList)
+            foreach (var item in commentDeleteList)
             {
                 db.Comments.Remove(item);
             }
@@ -71,7 +100,47 @@ namespace CapstoneProjectAdmin.API
             };
         }
 
+        [Route("AddQA")]
+        [HttpPost]
+        public HttpResponseMessage AddQA(QA qa)
+        {
+            db.QAs.Add(qa);
+            db.SaveChanges();
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new JsonContent(new
+                {
+                    success = true,
+                    message = "Add successful!",
+                })
+            };
+        }
+
+        [Route("DeleteQA")]
+        [HttpPost]
+        public HttpResponseMessage DeleteQA(QA qa)
+        {
+            var interactionDelete = db.Interactions.Where(e => e.QAId == qa.QAId);
+            foreach (var item in interactionDelete)
+            {
+                db.Interactions.Remove(item);
+            }
+            var qaDelete = db.QAs.Find(qa.QAId);
+            db.QAs.Remove(qaDelete);
+            db.SaveChanges();
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new JsonContent(new
+                {
+                    success = true,
+                    message = "Remove successful!",
+                })
+            };
+        }
+
     }
-
-
 }
