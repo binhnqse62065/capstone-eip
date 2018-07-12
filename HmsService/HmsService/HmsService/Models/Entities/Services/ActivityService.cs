b1143636732cdc136516108d17c564393e67e44d
binhnqse62065 <binhnqse62065@fpt.edu.vn>
2018-07-12 11:17:38 +0700

    + load động trang schedule cho client

---
+++ HmsService/HmsService/HmsService/Models/Entities/Services/ActivityService.cs
@@ -0,0 +1,28 @@
+﻿using HmsService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HmsService.Models.Entities.Services
+{
+    public partial interface IActivityService
+    {
+        IEnumerable<Activity> GetActivitiesBySessionId(int sessionId);
+    }
+    public partial class ActivityService
+    {
+        public IEnumerable<Activity> GetActivitiesBySessionId(int sessionId)
+        {
+            try
+            {
+                return this.Get(a => a.SessionId == sessionId).ToList();
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ HmsService/HmsService/HmsService/Sdk/ActivitytApi.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper.QueryableExtensions;
+using HmsService.Models;
+using HmsService.Models.Entities;
+using HmsService.ViewModels;
+using SkyWeb.DatVM.Mvc;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using HmsService.Sdk;
+
+namespace HmsService.Sdk
+{
+    public partial class ActivityApi
+    {
+        public IEnumerable<Activity> GetActivitiesBySessionId(int sessionId)
+        {
+            return this.BaseService.GetActivitiesBySessionId(sessionId);
+        }
+    }
+}
